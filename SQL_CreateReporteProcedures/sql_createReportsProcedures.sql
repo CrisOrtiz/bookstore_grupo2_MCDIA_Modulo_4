9cd8232f0bf5d1edf65fd17256156f4cb1b1b7df
alondra <griseldamh@gmail.com>
2026-08-16 19:39:30 -0400

    Agrego cambios Cubos OLAP

--- SQL_CreateReporteProcedures/sql_createReportsProcedures.sql
+++ SQL_CreateReporteProcedures/sql_createReportsProcedures.sql
@@ -30,3 +30,30 @@ END
 GO
 
 
+;
+
+
+CREATE  PROCEDURE sp_GetYears
+AS
+BEGIN
+	SELECT DISTINCT ddate.Year 
+	FROM [dbo].[DimDate]  ddate
+	WHERE ddate.Year>0
+	ORDER BY ddate.Year
+END
+
+;
+
+CREATE  OR ALTER PROCEDURE sp_GetShipMethod
+AS
+BEGIN
+	SELECT MethodName
+	FROM
+	(
+	SELECT CAST(NULL AS VARCHAR(100)) AS MethodName
+	UNION ALL
+	SELECT  dshmet.MethodName
+	FROM [dbo].[DimShippingMethod] dshmet  
+	) a
+	ORDER BY MethodName
+END
